d4a8ab22e217f2d7ece2ee28b7eef1d0e5257b09
Ross Bailey <Ross.Bailey@Fullscript.com>
2026-03-25 14:16:44 -0400

    fix bar chart data labels verticalization

--- dashboard_app.py
+++ dashboard_app.py
@@ -510,7 +510,7 @@ def main() -> None:
                 text=bar_lbl,
                 textposition="inside",
                 insidetextanchor="end",
-                textangle=-90,
+                textangle=90,
                 textfont=dict(family=FONT_FAMILY, color=CHART_TEXT, size=12),
             )
             fig.update_layout(
@@ -568,7 +568,7 @@ def main() -> None:
                 text=bar_lbl_hi,
                 textposition="inside",
                 insidetextanchor="end",
-                textangle=-90,
+                textangle=90,
                 textfont=dict(family=FONT_FAMILY, color=CHART_TEXT, size=12),
             )
             fig2.update_layout(
